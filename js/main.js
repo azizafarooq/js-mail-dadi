e15a38898cb55f0910a9bee822a558f1a8d4c6f6
azizafarooq <azizafarooq2000@gmail.com>
2024-01-14 17:54:34 +0100

    feat: cards

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const numberGenerator = document.getElementById('button');
 const result = document.getElementById('result');
+const userCard = document.getElementById('user-card');
+const computerCard = document.getElementById('computer-card');
 
 numberGenerator.addEventListener('click', function(){
     const userNumber = Math.floor(Math.random() * 6) + 1;
     const computerNumber = Math.floor(Math.random() * 6) + 1;
 
+    userCard.innerText= "Il tuo numero generato è: " + userNumber;
+    computerCard.innerText= "Quello computer è: " + computerNumber;
+    
     console.log(userNumber, computerNumber);
 
     if(userNumber > computerNumber){
